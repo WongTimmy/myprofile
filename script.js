26ee6ced9eccce6a913edaf521ab53b8a4a1f872
WongTimmy <102217804+WongTimmy@users.noreply.github.com>
2022-05-18 17:13:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,3 @@ $(document).ready(function() {
 $.extend($.scrollTo.defaults, {
      duration: 800,
 });
-$('#navpersonal').click(function() {
-     $.scrollTo('#head');
-});
-
-$('#navwork').click(function() {
-     $.scrollTo('#work');
-});
