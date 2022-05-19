8a5468fbbf658d5f3e09c62e707fed0aa1c0a31a
WongTimmy <102217804+WongTimmy@users.noreply.github.com>
2022-05-19 08:48:22 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-     $("#personal").addClass("animated slideInUp");
-     $(".principle").addClass("animated slideInUp");
-     $(".work").addClass("animated slideInUp");
-     $('.parallax').parallax();
-});
-$.extend($.scrollTo.defaults, {
-     duration: 800,
-});
